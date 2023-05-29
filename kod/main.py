f8befcb88136832186972f782c72663d1cd91dec
Andrzej Klusiewicz <andrzejklusiewicz33@gmail.com>
2023-05-29 10:04:09 +0200

    lama_in_my_living_room

--- kod/main.py
+++ kod/main.py
@@ -1,1 +1,26 @@
 #test
+# import time
+#
+# def fun():
+#     data=[1,2,3,4,5]
+#     time.sleep(5)
+#     return data
+
+# def fun():
+#     data=[1,2,3,4,5]
+#     for x in range(5):
+#         time.sleep(1)
+#         yield data[x]
+#     #time.sleep(5)
+#     #return data
+
+
+#przerwa do XX:XX
+
+#D3.js
+#FastAPI
+
+# for e in fun():
+#     print(e)
+
+print('whatever')
